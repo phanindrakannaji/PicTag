c423f137d484af71a02ea293444cdee76d8a06ca
phanindrakannaji <phanindrakannaji@gmail.com>
2017-05-08 22:37:53 -0400

    GetAllTags

--- WS/pictag/toggleTagForUser.php
+++ WS/pictag/toggleTagForUser.php
@@ -15,16 +15,18 @@ try{
 	    die("[ERROR] Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 	}
 
-	$query = "SELECT user_id, tag_id FROM user_tags 
-			WHERE user_id = $user_id AND tag_id = $tag_id";
+	$query = "SELECT user_id, tag_id FROM user_tags WHERE user_id = $user_id AND tag_id = $tag_id";	
 	$res = $mysqli->query($query);
-	if (!$res || $res->num_rows > 0){
-		$mysqli->query("DELETE FROM user_tags WHERE user_id = $user_id AND tag_id = $tag_id");
-		echo "Tag removed";
-	} else{
-		$mysqli->query("INSERT INTO user_tags(user_id, tag_id, notify, minUpVotes, created_date) VALUES($user_id, $tag_id, "N", 0, now())");
+	if (!$res || $res->num_rows == 0){
+		$insertQuery = "INSERT INTO user_tags(user_id, tag_id, notify, minUpVotes, created_date) VALUES($user_id, $tag_id, 'N', 0, now())";
+		$mysqli->query($insertQuery);
 		echo "Tag added to your list";
+	} else{
+		$deleteQuery = "DELETE FROM user_tags WHERE user_id = $user_id AND tag_id = $tag_id";
+		$mysqli->query($deleteQuery);
+		echo "Tag removed";
 	}
+	
 } catch (Exception $e) {
 	echo 'Caught exception: ',  $e->getMessage(), "\n";
 } finally{
